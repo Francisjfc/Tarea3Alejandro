439e49cb38f2dbf8aae71951c552ee9082350d58
Francis <francisco.fernandez@escuelaestech.es>
2024-02-22 16:34:48 +0100

    Creada carpeta backend

---
+++ Tarea3_Backend/src/test/java/com/example/Tarea3_Backend/Tarea3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tarea3_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
