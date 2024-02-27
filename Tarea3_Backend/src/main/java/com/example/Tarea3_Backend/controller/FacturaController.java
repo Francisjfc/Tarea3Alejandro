f6de87b34e086a9d348f48d3c4458c82bbbe0105
Francis <francisco.fernandez@escuelaestech.es>
2024-02-27 19:22:37 +0100

    Back Terminado

--- Tarea3_Backend/src/main/java/com/example/Tarea3_Backend/controller/FacturaController.java
+++ Tarea3_Backend/src/main/java/com/example/Tarea3_Backend/controller/FacturaController.java
@@ -3,6 +3,7 @@ package com.example.Tarea3_Backend.controller;
 import com.example.Tarea3_Backend.model.Factura;
 import com.example.Tarea3_Backend.service.FacturaService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class FacturaController {
 
     private FacturaService facturaService;
 
+    @PostMapping("/guardarFactura")
     public Factura guardarFacturas(Factura factura){
         return facturaService.guardarFactura(factura);
     }
--- Tarea3_Backend/src/main/java/com/example/Tarea3_Backend/service/ClienteService.java
+++ Tarea3_Backend/src/main/java/com/example/Tarea3_Backend/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.example.Tarea3_Backend.service;
 
+import com.example.Tarea3_Backend.Converter.ClienteFacturaConverter;
 import com.example.Tarea3_Backend.DTOs.ClienteFacturasDTO;
 import com.example.Tarea3_Backend.model.Cliente;
 import com.example.Tarea3_Backend.repository.ClienteRepository;
@@ -11,12 +12,15 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private ClienteFacturaConverter clienteFacturaConverter;
 
     public Cliente guardarCliente(Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     public ClienteFacturasDTO clienteFacturas(String nombre){
-        return clienteRepository.findByNombre(nombre);
+        Cliente cliente = clienteRepository.findByNombre(nombre);
+        return clienteFacturaConverter.convertEntityToDTO(cliente);
     }
 }
